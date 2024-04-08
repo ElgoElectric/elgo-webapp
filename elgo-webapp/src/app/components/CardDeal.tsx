e08d91fa51c55a99fde674691b0e4333b86b3444
rmurarishetti <64725000+rmurarishetti@users.noreply.github.com>
2024-04-08 12:20:46 +0800

    feat: Bug Fixes

--- elgo-webapp/src/app/components/CardDeal.tsx
+++ elgo-webapp/src/app/components/CardDeal.tsx
@@ -7,7 +7,7 @@ import * as ToggleGroup from "@radix-ui/react-toggle-group";
 import { productDesc } from "../constants";
 
 const toggleGroupItemClasses =
-  "color-mauve11 data-[state=on]:bg-gradient-to-r from-slate-600  flex h-[35px] w-auto p-2 items-center justify-center bg-slate rounded-md text-base leading-4 focus:z-10";
+  "data-[state=on]:bg-gradient-to-r from-slate-600  flex h-[35px] w-auto p-2 items-center justify-center bg-slate rounded-md text-base leading-4 focus:z-10";
 
 const CardDeal = () => {
   const [value, setValue] = React.useState("hvac");
@@ -23,79 +23,81 @@ const CardDeal = () => {
             reveal, and unlock savings.
           </div>
         </h2>
-        <div className="mt-4 mr-2">
-          <ToggleGroup.Root
-            className="inline-flex bg-gradient-to-r from-slate-800 to-black p-2 rounded space-x-px"
-            type="single"
-            value={value}
-            onValueChange={(value) => {
-              if (value) setValue(value);
-            }}
-            aria-label="Text alignment"
-          >
-            <ToggleGroup.Item
-              className={toggleGroupItemClasses}
-              value="hvac"
-              aria-label="Left aligned"
+        <div className="mt-4 mr-2 w-[95%]">
+          <div className="">
+            <ToggleGroup.Root
+              className="sm:flex lg:inline-flex xl:inline-flex 2xl:inline-flex bg-gradient-to-r from-slate-800 to-black p-2 rounded"
+              type="single"
+              value={value}
+              onValueChange={(value) => {
+                if (value) setValue(value);
+              }}
+              aria-label="Text alignment"
             >
-              <div
-                className={
-                  value == "hvac"
-                    ? "bg-gradient-to-r from-indigo-400 to-white text-transparent bg-clip-text"
-                    : "text-white"
-                }
+              <ToggleGroup.Item
+                className={toggleGroupItemClasses}
+                value="hvac"
+                aria-label="Left aligned"
               >
-                Air Conditioning
-              </div>
-            </ToggleGroup.Item>
-            <ToggleGroup.Item
-              className={toggleGroupItemClasses}
-              value="fridge"
-              aria-label="Center aligned"
-            >
-              <div
-                className={
-                  value == "fridge"
-                    ? "bg-gradient-to-r from-indigo-500 to-white text-transparent bg-clip-text"
-                    : "text-white"
-                }
+                <div
+                  className={
+                    value == "hvac"
+                      ? "bg-gradient-to-r from-indigo-400 to-white text-transparent bg-clip-text"
+                      : "text-white"
+                  }
+                >
+                  Air Conditioning
+                </div>
+              </ToggleGroup.Item>
+              <ToggleGroup.Item
+                className={toggleGroupItemClasses}
+                value="fridge"
+                aria-label="Center aligned"
               >
-                Refrigerator
-              </div>
-            </ToggleGroup.Item>
+                <div
+                  className={
+                    value == "fridge"
+                      ? "bg-gradient-to-r from-indigo-500 to-white text-transparent bg-clip-text"
+                      : "text-white"
+                  }
+                >
+                  Refrigerator
+                </div>
+              </ToggleGroup.Item>
 
-            <ToggleGroup.Item
-              className={toggleGroupItemClasses}
-              value="light"
-              aria-label="Right aligned"
-            >
-              <div
-                className={
-                  value == "light"
-                    ? "bg-gradient-to-r from-indigo-500 to-white text-transparent bg-clip-text"
-                    : "text-white"
-                }
+              <ToggleGroup.Item
+                className={toggleGroupItemClasses}
+                value="light"
+                aria-label="Right aligned"
               >
-                Lighting
-              </div>
-            </ToggleGroup.Item>
+                <div
+                  className={
+                    value == "light"
+                      ? "bg-gradient-to-r from-indigo-500 to-white text-transparent bg-clip-text"
+                      : "text-white"
+                  }
+                >
+                  Lighting
+                </div>
+              </ToggleGroup.Item>
 
-            <ToggleGroup.Item
-              className={toggleGroupItemClasses}
-              value="kapp"
-              aria-label="Right aligned"
-            >
-              <div
-                className={
-                  value == "kapp"
-                    ? "bg-gradient-to-r from-indigo-500 to-white text-transparent bg-clip-text"
-                    : "text-white"
-                }
+              <ToggleGroup.Item
+                className={toggleGroupItemClasses}
+                value="kapp"
+                aria-label="Right aligned"
               >
-                Kitchen Appliances
-              </div>
-            </ToggleGroup.Item>
-          </ToggleGroup.Root>
+                <div
+                  className={
+                    value == "kapp"
+                      ? "bg-gradient-to-r from-indigo-500 to-white text-transparent bg-clip-text"
+                      : "text-white"
+                  }
+                >
+                  Kitchen Appliances
+                </div>
+              </ToggleGroup.Item>
+            </ToggleGroup.Root>
+          </div>
           <div>
             <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
               {productDesc[value as keyof typeof productDesc]}
